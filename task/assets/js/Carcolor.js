4e44725a89c99c66b38b995d5f1a4eac669bc596
Anil Kumar Pandiya <anilpandiya05@gmail.com>
2018-10-15 10:04:39 +0530

    Add files via upload

---
+++ task/assets/js/Carcolor.js
@@ -0,0 +1,20 @@
+function changeToBlue(){
+    document.getElementById('car').src='./assets/img/blue.jpg';
+    document.getElementById('color').innerHTML = "Blue";
+}
+
+function changeToGrey(){
+    document.getElementById('car').src='./assets/img/grey.jpg';
+    document.getElementById('color').innerHTML = "Grey";
+}
+
+function changeToRed(){
+    document.getElementById('car').src='./assets/img/red.jpg';
+    document.getElementById('color').innerHTML = "Red";
+}
+
+function changeToSilver(){
+    document.getElementById('car').src='./assets/img/silver.jpg';
+    document.getElementById('color').innerHTML = "Silver";
+}
+
